bea807e9c85eef1f9f242d3306c1290215304561
Ogitsukasa <ogitsukasa55@gmail.com>
2025-01-01 22:10:54 +0900

    5章まで完了

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -1,45 +1,10 @@
 import Image from "next/image";
+import { getMembersList } from "@/app/_libs/microcms";
+import { MEMBERS_LIST_LIMIT } from "@/app/_constants";
 import styles from "./page.module.css";
 
-const data = {
-  contents: [
-    {
-      id: "1",
-      image: {
-        url: "/img-member1.jpg",
-        width: 240,
-        height: 240,
-      },
-      name: "デイビッドチャン",
-      position: "CEO",
-      profile: "グローバルテクノロジー",
-    },
-    {
-      id: "2",
-      image: {
-        url: "/img-member1.jpg",
-        width: 240,
-        height: 240,
-      },
-      name: "デイビッドチャン",
-      position: "CEO",
-      profile: "グローバルテクノロジー",
-    },
-    {
-      id: "3",
-      image: {
-        url: "/img-member1.jpg",
-        width: 240,
-        height: 240,
-      },
-      name: "デイビッドチャン",
-      position: "CEO",
-      profile: "グローバルテクノロジー",
-    },
-  ],
-};
-
-export default function Page() {
+export default async function Page() {
+  const data = await getMembersList({ limit: MEMBERS_LIST_LIMIT });
   return (
     <div className={styles.container}>
       {data.contents.length === 0 ? (
--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,21 @@
 import styles from "./page.module.css";
 import Image from "next/image";
-
+import { getNewsList } from "@/app/_libs/microcms";
+import { TOP_NEWS_LIMIT } from "@/app/_constants";
 import NewsList from "@/app/_components/NewsList";
 import ButtonLink from "@/app/_components/ButtonLink";
-import { News } from "@/app/_libs/microcms";
 
-const data: { contents: News[] } = {
-  contents: [
-    {
-      id: "1",
-      title: "渋谷にオフィスを移転",
-      category: {
-        name: "更新情報",
-      },
-      publishedAt: "2023/05/19",
-      createdAt: "2023/05/29",
-    },
-    {
-      id: "2",
-      title: "大阪にオフィスを移転",
-      category: {
-        name: "更新情報",
-      },
-      publishedAt: "2023/05/19",
-      createdAt: "2023/05/29",
-    },
-    {
-      id: "3",
-      title: "博多にオフィスを移転",
-      category: {
-        name: "更新情報",
-      },
-      publishedAt: "2023/05/19",
-      createdAt: "2023/05/29",
-    },
-  ],
-};
-
-export default function Home() {
-  const sliceData = data.contents.slice(0, 2);
-
-  const name = "世界";
+export default async function Home() {
+  const data = await getNewsList({
+    limit: TOP_NEWS_LIMIT,
+  });
 
   return (
     <>
       <section className={styles.top}>
         <div>
-          <h1 className={styles.title}>テクノロジー{name}</h1>
-          <p className={styles.description}>私たちは市場をリード</p>
+          <h1 className={styles.title}>SAMPLE</h1>
+          <p className={styles.description}>NEXT.JSサンプルページです</p>
         </div>
         <Image
           className={styles.bgimg}
@@ -59,7 +27,7 @@ export default function Home() {
       </section>
       <section className={styles.news}>
         <h2 className={styles.newsTitle}>News</h2>
-        <NewsList news={sliceData} />
+        <NewsList news={data.contents} />
         <div className={styles.newsLink}>
           <ButtonLink href="/news">もっとみる</ButtonLink>
         </div>
